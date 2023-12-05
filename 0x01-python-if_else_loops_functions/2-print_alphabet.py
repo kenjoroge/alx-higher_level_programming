da6693dae479171bed3a44cbd32b602dee8e6091
kenjoroge <njorogeken57@gmail.com>
2023-12-05 04:04:38 -0800

    Insomnia

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,2 @@
-#!/usr/bin/python3
-for Alphabet in range(ord('a'), ord('z') + 1):
-    # the ord function here I am using it to get the
-    # ascii code of the first and last lowercase
-    # The chr is being used to convvert the ascii code back to characters
-    print(chr(Alphabet), end=" ")
+for i in range(97, 123):
+    print("{}".format(chr(i)), end="")
