7e10e22ff07bc4fe85f4122326eb64025ceaa455
kenjoroge <njorogeken57@gmail.com>
2023-12-20 02:39:00 -0800

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,5 @@
 if __name__ == "__main__":
     import hidden_4
     for word in dir(hidden_4):
-        if not(word[0] == '_' and word[1] == '_'):
+        if not (word[0] == '_' and word[1] == '_'):
             print("{}".format(word))
